9f9ddc6bb047e14086e2579c39e96410c0a692b9
qiong.wu <qiong.wu@datayes.com>
2020-11-10 09:44:21 +0800

    tutor init

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mvn clean install -DskipTests=True
-scp target/mars-0.0.1-SNAPSHOT.jar pano@47.102.199.232:/data1/user/pano/projects/sh_msa/sh_msa_tmp.jar
-ssh pano@47.102.199.232 "cd /data1/user/pano/projects/sh_msa; rm sh_msa.jar.bak; mv sh_msa.jar sh_msa.jar.bak;mv sh_msa_tmp.jar sh_msa.jar"
-ssh pano@47.102.199.232 "ps -ef| grep 'sh_msa'|grep -v grep|cut -c 9-15|xargs kill -9"
-ssh pano@47.102.199.232 "cd /data1/user/pano/projects/sh_msa; nohup /usr/local/jdk/bin/java -jar sh_msa.jar --spring.config.location=./application.properties 1>out.txt 2>&1 &"
+scp target/mars-0.0.1-SNAPSHOT.jar pano@47.102.199.232:/data1/user/pano/projects/en_tutor/en_tutor_tmp.jar
+ssh pano@47.102.199.232 "cd /data1/user/pano/projects/en_tutor; rm en_tutor.jar.bak; mv en_tutor.jar en_tutor.jar.bak;mv en_tutor_tmp.jar en_tutor.jar"
+ssh pano@47.102.199.232 "ps -ef| grep 'en_tutor'|grep -v grep|cut -c 9-15|xargs kill -9"
+ssh pano@47.102.199.232 "cd /data1/user/pano/projects/en_tutor; nohup /usr/local/jdk/bin/java -jar en_tutor.jar --spring.config.location=./application.properties 1>out.txt 2>&1 &"
