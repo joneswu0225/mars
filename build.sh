e9e39de91e402efc01178e1298f9ae2dce55259c
qiong.wu <qiong.wu@datayes.com>
2020-11-10 14:16:34 +0800

    update

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mvn clean install -DskipTests=True
-scp target/mars-0.0.1-SNAPSHOT.jar pano@47.102.199.232:/data1/user/pano/projects/en_tutor/en_tutor_tmp.jar
-ssh pano@47.102.199.232 "cd /data1/user/pano/projects/en_tutor; rm en_tutor.jar.bak; mv en_tutor.jar en_tutor.jar.bak;mv en_tutor_tmp.jar en_tutor.jar"
+scp target/mars-0.0.1-SNAPSHOT.jar pano@47.102.199.232:/data1/project/tutor/backend/en_tutor_tmp.jar
+ssh pano@47.102.199.232 "cd /data1/project/tutor/backend; rm en_tutor.jar.bak; mv en_tutor.jar en_tutor.jar.bak;mv en_tutor_tmp.jar en_tutor.jar"
 ssh pano@47.102.199.232 "ps -ef| grep 'en_tutor'|grep -v grep|cut -c 9-15|xargs kill -9"
-ssh pano@47.102.199.232 "cd /data1/user/pano/projects/en_tutor; nohup /usr/local/jdk/bin/java -jar en_tutor.jar --spring.config.location=./application.properties 1>out.txt 2>&1 &"
+ssh pano@47.102.199.232 "cd /data1/project/tutor/backend; nohup /usr/local/jdk/bin/java -jar en_tutor.jar --spring.config.location=./application.properties 1>out.txt 2>&1 &"
